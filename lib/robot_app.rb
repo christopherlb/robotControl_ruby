548062a71956b89bfcee3ec449887b362ac33eeb
Chris Bruniges <chrisbruniges@gmail.com>
2025-07-07 11:04:18 +1000

    Simple place implementation

--- lib/robot_app.rb
+++ lib/robot_app.rb
@@ -9,10 +9,21 @@ class RobotApp
   def run
     @io.puts "Toy robot, waiting for input"
 
+    x = 0
+    y = 0
+    orientation = "E"
+
     loop do
       input = @io.gets&.strip&.downcase
       if input == "report"
-        @io.puts("0,0,E")
+        @io.puts("#{x},#{y},#{orientation}")
+      end
+
+      match = input&.match(/^place (\d+),(\d+),([nsew])$/i)
+      if match
+        x = match[1].to_i
+        y = match[2].to_i
+        orientation = match[3].upcase
       end
 
       break if input == "quit"
